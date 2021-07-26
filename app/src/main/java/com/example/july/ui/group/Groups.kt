cb7ac4332725956f989c803c56a5a62c029704c3
Ankit Gupta <ankitg0000640@gmail.com>
2021-07-26 23:27:20 +0530

    Multiple group chat code started

---
+++ app/src/main/java/com/example/july/ui/group/Groups.kt
@@ -0,0 +1,14 @@
+package com.example.july.ui.group
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.july.R
+
+class Groups : Fragment(R.layout.fragment_groups) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
